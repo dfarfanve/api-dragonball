d339ac1e0618b2421cd5da043df6316f793840b2
dfarfan <dfarfan@consisint.com>
2025-07-02 18:42:42 -0600

    Initial commit - API Dragonball project

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,12 +18,13 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
+      port: Number(process.env.MYSQL_PORT),
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      logging: true,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'images'),
